ef128bf30f0569b5d9d999f957ea91529931aa84
CelineBarbe <laloutrecosmique@hotmail.fr>
2022-02-10 15:39:19 +0100

    refacto et css

--- Niveau1/news.php
+++ Niveau1/news.php
@@ -5,26 +5,12 @@
         <title>ReSoC - Actualités</title> 
         <meta name="author" content="Julien Falconnet">
         <link rel="stylesheet" href="./style.css"/>
+        <meta name="make_beautiful_by" content="Laureuh et Loutrinette">
         <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap" rel="stylesheet">
     </head>
     <body>
-        <header>
-            <a href='admin.php'><img src="hedgehog.png" alt="Logo de notre réseau social"/></a>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">▾ Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-                </ul>
-            </nav>
-        </header>
+        <?php include './header.html'
+        ?>
         <div id="wrapper">
             <aside>
                 <img src="hedgehog_profile.png" alt="Portrait de l'utilisatrice"/>
